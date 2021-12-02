ab75038be1fd28aa90923f593960677908cadabe
keyboard2543 <keyboard2543@gmail.com>
2021-12-02 07:31:42 +0700

    change the api language

--- app.py
+++ app.py
@@ -1,162 +1,208 @@
-from model import *
-from flask import request
-from flasgger.utils import swag_from
-from flasgger import Swagger
-import json
+# from model import *
+# from flask import request
+# from flasgger.utils import swag_from
+# from flasgger import Swagger
+# import json
 
-app.config["SWAGGER"] = {"title": "ANIME-TV-API", "universion": 1}
+# app.config["SWAGGER"] = {"title": "ANIME-TV-API", "universion": 1}
 
-swagger_config = {
-    "headers": [],
-    "specs": [{
-        "title": "anime-tv-api",
-        "description":
-            "This is api documentation for anime.tv module",
-        "version": "1.5.2",
-        "externalDocs": {
-            "description": "See our github",
-            "url": "https://github.com/3-00AM/anime.tv-backend",
-        },
-        "servers": {
-            "url": "https://anime-tv-api.herokuapp.com/"
-        },
-        "endpoint": "api-doc",
-        "route": "/api",
-        "rule_filter": lambda rule: True,
-        "model_filter": lambda tag: True,
-    }],
-    "static_url_path": "/flasgger_static",
-    "swagger_ui": True,
-    "specs_route": "/api-doc/",
-}
-swagger = Swagger(app, config=swagger_config)
-
-
-@app.route('/', methods=['GET'])
-def hello_world():
-    return json.dumps({'feedback': 'End of the world!!! ~~ sono chi no kioku ~~'})
+# swagger_config = {
+#     "headers": [],
+#     "specs": [{
+#         "title": "anime-tv-api",
+#         "description":
+#             "This is api documentation for anime.tv module",
+#         "version": "1.5.2",
+#         "externalDocs": {
+#             "description": "See our github",
+#             "url": "https://github.com/3-00AM/anime.tv-backend",
+#         },
+#         "servers": {
+#             "url": "https://anime-tv-api.herokuapp.com/"
+#         },
+#         "endpoint": "api-doc",
+#         "route": "/api",
+#         "rule_filter": lambda rule: True,
+#         "model_filter": lambda tag: True,
+#     }],
+#     "static_url_path": "/flasgger_static",
+#     "swagger_ui": True,
+#     "specs_route": "/api-doc/",
+# }
+# swagger = Swagger(app, config=swagger_config)
 
 
-@app.route('/index', methods=['GET'])
-def index():
-    return hello_world()
+# @app.route('/', methods=['GET'])
+# def hello_world():
+#     return json.dumps({'feedback': 'End of the world!!! ~~ sono chi no kioku ~~'})
 
 
-@app.route('/anime', methods=['GET'])
-@swag_from("swagger/anime_get.yml")
-def anime():
-    anime_list = []
-    for anime in db.session.query(Anime).all():
-        anime_list.append(anime.get_dict())
-    return json.dumps(anime_list)
+# @app.route('/index', methods=['GET'])
+# def index():
+#     return hello_world()
 
 
-@app.route('/anime/search', methods=['GET'])
-@swag_from("swagger/anime_search_get.yml")
-def anime_search():
-    keyword = request.args.get('keyword')
-    anime_list = []
-    for anime in db.session.query(Anime).all():
-        anime_dict = anime.get_dict()
-        if keyword.lower() in anime_dict['title'].lower():
-            anime_list.append(anime_dict)
-            continue
-        key_check_list = {
-            'genres': 'name',
-            'studios': 'name',
-            'related_anime': 'title',
-            'related_manga': 'title',
-            'recommendations': 'title',
-            'related_theme': 'title',
-        }
-        for key, checkee in key_check_list.items():
-            for keyee in anime_dict[key]:
-                if keyword in keyee[checkee].lower():
-                    anime_list.append(anime_dict)
-                    continue
-
-    return json.dumps(anime_list)
+# @app.route('/anime', methods=['GET'])
+# @swag_from("swagger/anime_get.yml")
+# def anime():
+#     anime_list = []
+#     for anime in db.session.query(Anime).all():
+#         anime_list.append(anime.get_dict())
+#     return json.dumps(anime_list)
 
 
-@app.route('/genre', methods=['GET'])
-@swag_from("swagger/genre_get.yml")
-def genre():
-    genre_list = []
-    for g in db.session.query(Genre).all():
-        genre_list.append(g.get_dict())
-    return json.dumps(genre_list)
+# @app.route('/anime/search', methods=['GET'])
+# @swag_from("swagger/anime_search_get.yml")
+# def anime_search():
+#     keyword = request.args.get('keyword')
+#     anime_list = []
+#     for anime in db.session.query(Anime).all():
+#         anime_dict = anime.get_dict()
+#         if keyword.lower() in anime_dict['title'].lower():
+#             anime_list.append(anime_dict)
+#             continue
+#         key_check_list = {
+#             'genres': 'name',
+#             'studios': 'name',
+#             'related_anime': 'title',
+#             'related_manga': 'title',
+#             'recommendations': 'title',
+#             'related_theme': 'title',
+#         }
+#         for key, checkee in key_check_list.items():
+#             for keyee in anime_dict[key]:
+#                 if keyword in keyee[checkee].lower():
+#                     anime_list.append(anime_dict)
+#                     continue
+
+#     return json.dumps(anime_list)
 
 
-@app.route('/genre/search', methods=['GET'])
-@swag_from("swagger/genre_search_get.yml")
-def genre_search():
-    keyword = request.args.get('keyword')
-    genre_list = []
-    for g in db.session.query(Genre).all():
-        if keyword.lower() in g.name.lower():
-            genre_list.append(g.get_dict())
-    return json.dumps(genre_list)
+# @app.route('/genre', methods=['GET'])
+# @swag_from("swagger/genre_get.yml")
+# def genre():
+#     genre_list = []
+#     for g in db.session.query(Genre).all():
+#         genre_list.append(g.get_dict())
+#     return json.dumps(genre_list)
 
 
-@app.route('/studio', methods=['GET'])
-@swag_from("swagger/studio_get.yml")
-def studio():
-    studio_list = []
-    for studio in db.session.query(Studio).all():
-        studio_list.append(studio.get_dict())
-    return json.dumps(studio_list)
+# @app.route('/genre/search', methods=['GET'])
+# @swag_from("swagger/genre_search_get.yml")
+# def genre_search():
+#     keyword = request.args.get('keyword')
+#     genre_list = []
+#     for g in db.session.query(Genre).all():
+#         if keyword.lower() in g.name.lower():
+#             genre_list.append(g.get_dict())
+#     return json.dumps(genre_list)
 
 
-@app.route('/studio/search', methods=['GET'])
-@swag_from("swagger/studio_search_get.yml")
-def studio_search():
-    keyword = request.args.get('keyword')
-    studio_list = []
-    for studio in db.session.query(Studio).all():
-        if keyword.lower() in studio.name.lower():
-            studio_list.append(studio.get_dict())
-    return json.dumps(studio_list)
+# @app.route('/studio', methods=['GET'])
+# @swag_from("swagger/studio_get.yml")
+# def studio():
+#     studio_list = []
+#     for studio in db.session.query(Studio).all():
+#         studio_list.append(studio.get_dict())
+#     return json.dumps(studio_list)
 
 
-@app.route('/manga', methods=['GET'])
-@swag_from("swagger/manga_get.yml")
-def manga():
-    manga_list = []
-    for manga in db.session.query(Manga).all():
-        manga_list.append(manga.get_dict())
-    return json.dumps(manga_list)
+# @app.route('/studio/search', methods=['GET'])
+# @swag_from("swagger/studio_search_get.yml")
+# def studio_search():
+#     keyword = request.args.get('keyword')
+#     studio_list = []
+#     for studio in db.session.query(Studio).all():
+#         if keyword.lower() in studio.name.lower():
+#             studio_list.append(studio.get_dict())
+#     return json.dumps(studio_list)
 
 
-@app.route('/manga/search', methods=['GET'])
-@swag_from("swagger/manga_search_get.yml")
-def manga_search():
-    keyword = request.args.get('keyword')
-    manga_list = []
-    for manga in db.session.query(Manga).all():
-        if keyword.lower() in manga.title.lower():
-            manga_list.append(manga.get_dict())
-    return json.dumps(manga_list)
+# @app.route('/manga', methods=['GET'])
+# @swag_from("swagger/manga_get.yml")
+# def manga():
+#     manga_list = []
+#     for manga in db.session.query(Manga).all():
+#         manga_list.append(manga.get_dict())
+#     return json.dumps(manga_list)
 
 
-@app.route('/theme', methods=['GET'])
-@swag_from("swagger/theme_get.yml")
-def theme():
-    theme_list = []
-    for theme in db.session.query(Theme).all():
-        theme_list.append(theme.get_dict())
-    return json.dumps(theme_list)
+# @app.route('/manga/search', methods=['GET'])
+# @swag_from("swagger/manga_search_get.yml")
+# def manga_search():
+#     keyword = request.args.get('keyword')
+#     manga_list = []
+#     for manga in db.session.query(Manga).all():
+#         if keyword.lower() in manga.title.lower():
+#             manga_list.append(manga.get_dict())
+#     return json.dumps(manga_list)
 
 
-@app.route('/theme/search', methods=['GET'])
-@swag_from("swagger/theme_search_get.yml")
-def theme_search():
-    keyword = request.args.get('keyword')
-    theme_list = []
-    for theme in db.session.query(Theme).all():
-        if keyword.lower() in theme.title.lower():
-            theme_list.append(theme.get_dict())
-    return json.dumps(theme_list)
+# @app.route('/theme', methods=['GET'])
+# @swag_from("swagger/theme_get.yml")
+# def theme():
+#     theme_list = []
+#     for theme in db.session.query(Theme).all():
+#         theme_list.append(theme.get_dict())
+#     return json.dumps(theme_list)
+
+
+# @app.route('/theme/search', methods=['GET'])
+# @swag_from("swagger/theme_search_get.yml")
+# def theme_search():
+#     keyword = request.args.get('keyword')
+#     theme_list = []
+#     for theme in db.session.query(Theme).all():
+#         if keyword.lower() in theme.title.lower():
+#             theme_list.append(theme.get_dict())
+#     return json.dumps(theme_list)
+
+
+# if __name__ == '__main__':
+#     app.run()
+
+from openapi_server import encoder
+import sys
+import os
+
+if not os.path.exists("config.py"):
+    print("Configuration 'config.py' not found.  "
+          "You may create one from 'config.py.example'.")
+    exit(1)
+
+from config import OPENAPI_AUTOGEN_DIR
+
+if not os.path.exists(OPENAPI_AUTOGEN_DIR):
+    print("Folder '{}' not found.  "
+          "Execute openapi-generator to generate it, "
+          "e.g.,".format(OPENAPI_AUTOGEN_DIR))
+    print()
+    print("  "
+          "java -jar openapi-generator-cli-X.Y.Z.jar generate \\\n"
+          "     -i openapi/rain-api.yaml \\\n"
+          "     -o {} -g python-flask".format(OPENAPI_AUTOGEN_DIR))
+    print()
+    exit(1)
+
+sys.path.append(OPENAPI_AUTOGEN_DIR)
+
+try:
+    import connexion
+except ModuleNotFoundError:
+    print("Please install all required packages by running:"
+          " pip install -r requirements.txt")
+    exit(1)
+
+
+def main():
+    app = connexion.App(__name__, specification_dir='./openapi/')
+    app.app.json_encoder = encoder.JSONEncoder
+    app.add_api('anime-api.yaml',
+                arguments={'title': 'Anime.tv API'},
+                pythonic_params=True)
+
+    app.run(port=8080, debug=True)
 
 
 if __name__ == '__main__':
-    app.run()
+    main()
